d1f0d52e86f631e11e07cb76922d94f717aff818
VannaKarenina <46137062+VannaKarenina@users.noreply.github.com>
2023-01-10 10:54:51 +0300

    Config file

---
+++ config/initial.config.ts
@@ -0,0 +1,11 @@
+export default {
+    hero: {
+        avatar_url: 'https://ik.imagekit.io/vannakarenina/kiinse/kiinse1__2__ZbeHJ2KcR.png?ik-sdk-version=javascript-1.4.3&updatedAt=1665491170987'
+    },
+    socialLinks: [
+        {
+            icon: 'github',
+            url: 'https://github.com'
+        }
+    ]
+}
