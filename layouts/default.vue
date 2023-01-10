a9a33c8f7590442bef2eb5ff2d32178886d10b7e
VannaKarenina <46137062+VannaKarenina@users.noreply.github.com>
2023-01-10 10:55:24 +0300

    Default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,16 @@
 <template>
-$END$
+  <div>
+    <NavigationBase/>
+    <slot/>
+  </div>
 </template>
 
 <script>
+import NavigationBase from "~/components/Base/NavigationBase.vue";
+
 export default {
-name: "default"
+  name: "default",
+  components: {NavigationBase}
 }
 </script>
 
