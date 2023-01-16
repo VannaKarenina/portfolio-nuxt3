467e5d141bccc38ee3a1ba1a8e58d596a63a2e5f
VannaKarenina <46137062+VannaKarenina@users.noreply.github.com>
2023-01-16 12:31:23 +0300

    Info config files additions

---
+++ config/projectsList.config.ts
@@ -0,0 +1,26 @@
+export const projectsListConfig = [
+    {
+        title: 'ThirstForWater',
+        imageLink: 'thirstforwater.png',
+        tech: [
+            {
+                tooltip: 'Github',
+                icon: 'github'
+            }
+        ],
+        readmePath: '/projects/thirstforwater',
+        liveLink: 'https://github.com/NubilumDev/ThirstForWater'
+    },
+    {
+        title: 'TelegramPKGH',
+        imageLink: 'pkghlogo.jpg',
+        tech: [
+            {
+                tooltip: 'Github',
+                icon: 'github'
+            }
+        ],
+        readmePath: '/projects/telegrampkgh',
+        liveLink: 'https://t.me/pkghschedulebot'
+    },
+]
